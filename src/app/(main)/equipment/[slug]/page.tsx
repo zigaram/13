64352abf326e9901bc58a98953005becf3021d45
zigaram <ziga.ramone@gmail.com>
2026-03-18 23:18:35 +0100

    fix tank size imageUrl

--- src/app/(main)/equipment/[slug]/page.tsx
+++ src/app/(main)/equipment/[slug]/page.tsx
@@ -227,7 +227,6 @@ export default async function EquipmentReviewPage({ params }: Props) {
               />
 
               <div className="ad-slot-sidebar" data-ad-slot="equipment-sidebar">Ad</div>
-              </div>
             </div>
           </aside>
         </div>
